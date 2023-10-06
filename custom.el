e8997eb0f4f3001d4920f162ba4ed07e019f9a6e
Gregory Faruch <gregory@retailshake.com>
2023-10-06 11:44:49 +0200

    update

--- custom.el
+++ custom.el
@@ -3,8 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages
-   '(terraform-doc terraform-mode sly-asdf lsp-pyright dockerfile-mode company-terraform google-translate vagrant-tramp vagrant org-roam modus-themes lsp-treemacs)))
+   '(jinja2-mode terraform-doc terraform-mode sly-asdf lsp-pyright dockerfile-mode company-terraform google-translate vagrant-tramp vagrant org-roam modus-themes lsp-treemacs)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
